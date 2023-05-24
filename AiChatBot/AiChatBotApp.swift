2cd05feed812edb0ff2ae60caf8d1dd21391811c
Irtaza Fiaz <irtaza@iiinigence.com>
2023-05-25 00:51:31 +0500

    Project Setup

---
+++ AiChatBot/AiChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiChatBotApp.swift
+//  AiChatBot
+//
+//  Created by Irtaza Fiaz on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AiChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
