da26c30bcf9af6da6ec16ff84362f16f49905a02
irtazafayaz <irtazafayaz@gmail.com>
2023-09-11 18:06:11 +0500

    -- PopupView -- Websocket

---
+++ AiChatBot/Utils/Widgets/PopupView.swift
@@ -0,0 +1,20 @@
+//
+//  PopupView.swift
+//  AiChatBot
+//
+//  Created by Irtaza Fiaz on 11/09/2023.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopupView()
+    }
+}
---
+++ AiChatBot/ViewModels/WebSocket.swift
@@ -0,0 +1,8 @@
+//
+//  WebSocket.swift
+//  AiChatBot
+//
+//  Created by Irtaza Fiaz on 11/09/2023.
+//
+
+import Foundation
